b0f3c331b83debfffe7cf05c04d75ad6a46fd042
Lucas dos Santos Antunes <lucas.antunes.santos81@gmail.com>
2024-05-27 20:06:48 -0300

    Entrega 3

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
-
 const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
+const { createServer } = require('http');
+const { join } = require('path');
 const { Server } = require('socket.io');
+const fs = require('fs');
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+let perguntas = [];
+let pontos_jogador = {};
+
+
+fs.readFile(join(__dirname, 'perguntas.json'), 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    perguntas = JSON.parse(data);
+});
+
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
-
 });
 
+io.on('connection', (socket) => {
+    console.log('Um usuário conectou-se');
+    pontos_jogador[socket.id] = 0;
 
-server.on('connection', (socket) => {
-    console.log('Funcionou!!');
-    
- 
+    const enviarPergunta = () => {
+        const pergunta = perguntas[Math.floor(Math.random() * perguntas.length)];
+        socket.emit('pergunta', pergunta);
+    };
+
+    enviarPergunta();
+
+    socket.on('resposta', (data) => {
+        const { pergunta, resposta } = data;
+        const perguntacorreta = perguntas.find(q => q.pergunta === pergunta);
+        if (perguntacorreta && perguntacorreta.resposta === resposta) {
+            pontos_jogador[socket.id] += 1;
+            socket.emit('result', { correct: true });
+        } else {
+            socket.emit('result', { correct: false });
+        }
+
+        socket.emit('pontos_jogador', { pontos_jogador: pontos_jogador[socket.id] });
+
+
+        enviarPergunta();
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Um usuário desconectou-se');
+        delete pontos_jogador[socket.id];
+    });
 });
-server.on('disconect',(socket)=>{
-    console.log('Disconect');
-})
 
 server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
+    console.log('Server running at http://localhost:3000');
 });
